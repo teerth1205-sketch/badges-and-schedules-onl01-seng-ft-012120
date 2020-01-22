66ca35fb2af925cfc665291245de69965432d0ba
Teerth Patel <teerth1205@gmail.com>
2020-01-22 23:09:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arrays =[]
+  array.each do |name|
+    arrays << "Hello, my name is #{name}."
+  end
+  return arrays
+end
+
+def assign_rooms(array)
+  arrays =[]
+  room = 1
+  array.each do |name|
+    arrays << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return arrays
+end
+
+def printer(array)
+  result_1 = batch_badge_creator(array)
+  
+  result_1.each do |x|
+    puts x
+  end
+  result_2 = assign_rooms(array)
+  result_2.each do |x|
+    puts x
+  end
+end
+
+
+      
